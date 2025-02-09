f7828826b23921d1421fabbbcbd17e3b164fdb7b
hamoudhamidouche <hamoudabdelbasset.hamidouche@gmail.com>
2025-02-09 07:34:11 +0100

    adding ui

--- script.js
+++ script.js
@@ -9,92 +9,106 @@ function getComputerChoice() {
     else {
         return "scissors";
     }
-}
+};
 
-function getHumanChoice() {
-    while(true) {
-        console.log("Choose one of these choices: \n 1.rock\n 2.paper \n 3.scissors\n");
-        let choice = prompt("Enter your choice : ");
+// function getHumanChoice() {
+//     while(true) {
+//         console.log("Choose one of these choices: \n 1.rock\n 2.paper \n 3.scissors\n");
+//         let choice = prompt("Enter your choice : ");
 
-        if (choice == 1) {
-            return "rock";
+//         if (choice == 1) {
+//             return "rock";
+//         }
+//         else if (choice == 2) {
+//             return "paper";
+//         }
+//         else if (choice == 3) {
+//             return "scissors";
+//         }
+//         else {
+//             alert("Invalid choice");
+//         }
+//     }
+// };
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == "rock") {
+        if (computerChoice == "rock") {
+            alert("It's a tie");
+            return 0;
         }
-        else if (choice == 2) {
-            return "paper";
-        }
-        else if (choice == 3) {
-            return "scissors";
+        else if (computerChoice == "paper") {
+            alert("You lose!");
+            return 1;
         }
         else {
-            alert("Invalid choice");
+            alert("You win!");
+            return 2;
+        }
+    }
+    else if (humanChoice == "paper") {
+        if (computerChoice == "paper") {
+            alert("It's a tie");
+            return 0;
+        }
+        else if (computerChoice == "scissors") {
+            alert("You lose!");
+            return 1;
+        }
+        else {
+            alert("You win!");
+            return 2;
+        }
+    }
+    else if (humanChoice == "scissors") {
+        if (computerChoice == "scissors") {
+            alert("It's a tie");
+            return 0;
+        }
+        else if (computerChoice == "rock") {
+            alert("You lose!");
+            return 1;
+        }
+        else {
+            alert("You win!");
+            return 2;
         }
     }
 }
 
-function playGame() {
-    function playRound(humanChoice, computerChoice) {
-        if (humanChoice == "rock") {
-            if (computerChoice == "rock") {
-                alert("It's a tie");
-                return 0;
-            }
-            else if (computerChoice == "paper") {
-                alert("You lose!");
-                return 1;
-            }
-            else {
-                alert("You win!");
-                return 2;
-            }
-        }
-        else if (humanChoice == "paper") {
-            if (computerChoice == "paper") {
-                alert("It's a tie");
-                return 0;
-            }
-            else if (computerChoice == "scissors") {
-                alert("You lose!");
-                return 1;
-            }
-            else {
-                alert("You win!");
-                return 2;
-            }
-        }
-        else if (humanChoice == "scissors") {
-            if (computerChoice == "scissors") {
-                alert("It's a tie");
-                return 0;
-            }
-            else if (computerChoice == "rock") {
-                alert("You lose!");
-                return 1;
-            }
-            else {
-                alert("You win!");
-                return 2;
-            }
-        }
-    }
+const sect = document.querySelector("section");
+
+const btnOne = document.createElement("button");
+btnOne.textContent = "Rock";
+const btnTwo = document.createElement("button");
+btnTwo.textContent = "Paper";
+const btnThree = document.createElement("button");
+btnThree.textContent = "scissors";
+
+
+
+btnOne.addEventListener("click", playRound("rock", getComputerChoice));
+btnTwo.addEventListener("click", playRound("paper", getComputerChoice));
+btnThree.addEventListener("click", playRound("scissors", getComputerChoice));
+
+sect.appendChild(btnOne);
+sect.appendChild(btnTwo);
+sect.appendChild(btnThree);
+
     
-    let humanScore = 0;
-    let computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
-    for (let i = 0; i<5; i++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        console.log(`Computer choose ${computerSelection}`)
-        let final = playRound(humanSelection, computerSelection);
-        
-        if (final == 1) {
-            computerScore++;
-        }
-        else if (final == 2) {
-            humanScore++;
-        }
-        console.log(`Your score : ${humanScore} \nComputer score : ${computerScore}`);
-    }
-    console.log("\nThanks for playing");
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+console.log(`Computer choose ${computerSelection}`)
+let final = playRound(humanSelection, computerSelection);
+    
+if (final == 1) {
+    computerScore++;
 }
-
-playGame();
+else if (final == 2) {
+    humanScore++;
+}
+console.log(`Your score : ${humanScore} \nComputer score : ${computerScore}`);
+console.log("\nThanks for playing");
